556c5e05a7e4037065091c03d8f635773644e024
Shadownc <365172043@qq.com>
2023-09-27 14:47:01 +0800

    Update CommonScript.js

--- components/CommonScript.js
+++ components/CommonScript.js
@@ -9,14 +9,15 @@ const CommonScript = () => {
   return (<>
 
     {BLOG.CHATBASE_ID && (<>
-      <script id={BLOG.CHATBASE_ID} src="https://www.chatbase.co/embed.min.js" defer />
-      <script async dangerouslySetInnerHTML={{
-        __html: `
-             window.chatbaseConfig = {
+        <script chatbotId={BLOG.CHATBASE_ID} src="https://www.chatbase.co/embed.min.js" domain="www.chatbase.co" defer/>
+        <script async dangerouslySetInnerHTML={{
+          __html: `
+             window.embeddedChatbotConfig = {
                 chatbotId: "${BLOG.CHATBASE_ID}",
+                domain: "www.chatbase.co"
             }
         `
-      }} />
+        }}/>
     </>)}
 
     {BLOG.COMMENT_DAO_VOICE_ID && (<>
@@ -42,22 +43,22 @@ const CommonScript = () => {
 
     {BLOG.COMMENT_CUSDIS_APP_ID && <script defer src={`https://cusdis.com/js/widget/lang/${BLOG.LANG.toLowerCase()}.js`} />}
 
-    {BLOG.COMMENT_TWIKOO_ENV_ID && <script defer src={BLOG.COMMENT_TWIKOO_CDN_URL} />}
+    {BLOG.COMMENT_TWIKOO_ENV_ID && <script defer src={BLOG.COMMENT_TWIKOO_CDN_URL}/> }
 
-    {BLOG.COMMENT_ARTALK_SERVER && <script defer src={BLOG.COMMENT_ARTALK_JS} />}
+    {BLOG.COMMENT_ARTALK_SERVER && <script defer src={BLOG.COMMENT_ARTALK_JS}/> }
 
     {BLOG.COMMENT_TIDIO_ID && <script async src={`//code.tidio.co/${BLOG.COMMENT_TIDIO_ID}.js`} />}
 
     {/* gitter聊天室 */}
     {BLOG.COMMENT_GITTER_ROOM && (<>
-      <script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer />
+      <script src="https://sidecar.gitter.im/dist/sidecar.v1.js" async defer/>
       <script async dangerouslySetInnerHTML={{
         __html: `
                 ((window.gitter = {}).chat = {}).options = {
                   room: '${BLOG.COMMENT_GITTER_ROOM}'
                 };
                 `
-      }} />
+      }}/>
     </>)}
 
     {/* 代码统计 */}
@@ -72,8 +73,8 @@ const CommonScript = () => {
     {/* 百度统计 */}
     {BLOG.ANALYTICS_BAIDU_ID && (
       <script async
-        dangerouslySetInnerHTML={{
-          __html: `
+              dangerouslySetInnerHTML={{
+                __html: `
               var _hmt = _hmt || [];
               (function() {
                 var hm = document.createElement("script");
@@ -82,29 +83,29 @@ const CommonScript = () => {
                 s.parentNode.insertBefore(hm, s);
               })();
               `
-        }}
+              }}
       />
     )}
 
     {/* 站长统计 */}
     {BLOG.ANALYTICS_CNZZ_ID && (
       <script async
-        dangerouslySetInnerHTML={{
-          __html: `
+              dangerouslySetInnerHTML={{
+                __html: `
               document.write(unescape("%3Cspan style='display:none' id='cnzz_stat_icon_${BLOG.ANALYTICS_CNZZ_ID}'%3E%3C/span%3E%3Cscript src='https://s9.cnzz.com/z_stat.php%3Fid%3D${BLOG.ANALYTICS_CNZZ_ID}' type='text/javascript'%3E%3C/script%3E"));
               `
-        }}
+              }}
       />
     )}
 
     {/* 谷歌统计 */}
     {BLOG.ANALYTICS_GOOGLE_ID && (<>
       <script async
-        src={`https://www.googletagmanager.com/gtag/js?id=${BLOG.ANALYTICS_GOOGLE_ID}`}
+              src={`https://www.googletagmanager.com/gtag/js?id=${BLOG.ANALYTICS_GOOGLE_ID}`}
       />
       <script async
-        dangerouslySetInnerHTML={{
-          __html: `
+              dangerouslySetInnerHTML={{
+                __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -112,25 +113,8 @@ const CommonScript = () => {
                       page_path: window.location.pathname,
                     });
                   `
-        }}
+              }}
       />
-    </>)}
-
-    {/* tawk聊天 */}
-    {BLOG.TAWK_ID && (<>
-      <script async dangerouslySetInnerHTML={{
-        __html: `
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/${BLOG.TAWK_ID}';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-          })();
-        `
-      }} />
     </>)}
 
     {/* 引入音乐播放 */}
